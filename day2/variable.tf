6e038e06b5ed6a9bb6ba12713b8a05a3b5db9884
Your Name <your-email@gmail.com>
2025-12-31 15:20:14 +0530

    update

---
+++ day2/variable.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+    description = "passing value to main"
+    type = string
+    default = ""
+  
+}
+variable "type" {
+    type = string
+    default = ""
+  
+}
+variable "tags" {
+    type = string
+    default = ""
+}
+  
