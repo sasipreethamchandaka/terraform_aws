56dd68b4c360379794bdb131aff08a4a86325e55
Your Name <your-email@gmail.com>
2026-01-03 05:32:48 +0530

    update

--- day-4-state/main.tf
+++ day-4-state/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name" {
     ami = "ami-068c0051b15cdb816"
     instance_type = "t2.medium"
-   #tags = {
-     #Name = "sasi-chandaka"
+   tags = {
+     Name = "sasi-chandaka"
    }
-
+}
