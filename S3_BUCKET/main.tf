7a0094d39d547289afe8b97583e8fa1f2eb1a967
Your Name <your-email@gmail.com>
2026-01-24 14:54:05 +0530

    s3-bucket  created in different names in same region

---
+++ S3_BUCKET/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test_buckets" {
+  count         = length(var.s3_bucket_names) //count will be 3
+  bucket        = var.s3_bucket_names[count.index]
+  force_destroy = true
+}
---
+++ S3_BUCKET/provider.tf
@@ -0,0 +1,4 @@
+variable "s3_bucket_names" {
+  type = list
+  default = ["deve-bucket.app", "testest-bucket.app", "prodprod-bucket.app"]
+}
